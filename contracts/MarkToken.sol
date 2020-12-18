9c0607362f61b624f6a73a108b1c14c9ba71fbd8
Marcos Miranda <marcos.miranda.1995@gmail.com>
2020-12-17 22:17:09 -0300

    [ADD] - Delegated trasnfers

--- contracts/MarkToken.sol
+++ contracts/MarkToken.sol
@@ -14,7 +14,15 @@ contract MarkToken {
         uint256 _value
     );
 
+    event Approval(
+        // Declaration of a "approval" event that is triggered when a delegated transfer is approved. Required by ERC-20
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor (uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
@@ -26,10 +34,35 @@ contract MarkToken {
         require(balanceOf[msg.sender] >= _value); // Checks that the address has enough tokens. Required by ERC-20
         
         balanceOf[msg.sender] -= _value; // Deducts funds from the sender's account
+        
         balanceOf[_to] += _value; // Adds funds to the receiver's account
 
         emit Transfer(msg.sender, _to, _value); // Triggers the "Transfer" event. Required by ERC-20
 
-        return true; //Retuns success true
+        return true; // Retuns success true. Required by ERC-20
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        
+        allowance[msg.sender][_spender] = _value;
+        
+        emit Approval(msg.sender, _spender, _value); // Triggers the "Approval" event. Required by ERC-20
+        
+        return true; // Retuns success true. Required by ERC-20
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        
+        require(_value <= balanceOf[_from]); //checks that the fromAccount has enough funds
+        require(_value <= allowance[_from][msg.sender]); //checks that the fromAccount has enough allowance
+
+        balanceOf[_from] -= _value; // Deducts from the fromAccount
+        balanceOf[_to] += _value; // Adds to the toAccount
+
+        allowance[_from][msg.sender] -= _value; // If delegated trasnfer is sucessful, updates the allowance
+
+        emit Transfer(_from, _to, _value); // Triggers the "Transfer" event. Required by ERC-20
+        
+        return true; // Retuns success true. Required by ERC-20
     }
 }
