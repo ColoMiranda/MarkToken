df9e2891843f5d0a23213435f0daffd7be13bc86
Marcos Miranda <marcos.miranda.1995@gmail.com>
2020-12-19 16:41:10 -0300

    [ADD] - Final MakeSaleToken contract functions

--- contracts/MarkTokenSale.sol
+++ contracts/MarkTokenSale.sol
@@ -5,7 +5,7 @@ import "./MarkToken.sol";
 
 contract MarkTokenSale {
 
-    address admin; // declares the contract admin address
+    address payable private admin; // declares the contract admin address
     MarkToken public tokenContract; // declares MarkToken.sol as the token contract
     uint256 public tokenPrice; // declares the token price
     uint256 public tokenSold; // declares a variable that keeps track of the amount fo tokens sold
@@ -24,11 +24,33 @@ contract MarkTokenSale {
         tokenPrice = _tokenPrice; // sets the token price to the value passed during the migration
     }
 
+    //Safe Math multiply function
+    function multiply(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x);
+    }
+    
     function buyTokens(uint256 _numberOfTokens) public payable {
 
+        require(tokenContract.balanceOf(address(this)) >= _numberOfTokens); // checks that the contract in charge of handling the sale has enough
+
+        require(msg.value == multiply(_numberOfTokens, tokenPrice));
+
+        require(tokenContract.transfer(msg.sender, _numberOfTokens));
+        
         tokenSold += _numberOfTokens;
 
         emit Sell(msg.sender, _numberOfTokens);
+
+    }
+
+    
+    function endSale() public {
+        
+        require(msg.sender == admin);
+
+        require(tokenContract.transfer(admin, tokenContract.balanceOf(address(this))));
+
+        selfdestruct(admin);
 
     }
 }
--- test/MarkTokenSale.js
+++ test/MarkTokenSale.js
@@ -1,10 +1,15 @@
 var MarkTokenSale = artifacts.require("./MarkTokenSale.sol");
+var MarkToken = artifacts.require("./MarkToken.sol");
 
 contract("MarkTokenSale", function (accounts) {
   var tokenSaleInstance;
+  var tokenInstance;
   var tokenPrice = 1000000000000000; // in wei
+  var admin = accounts[0];
   var buyer = accounts[1];
+  var tokensAvailable = 750000;
   var numberOfTokens = 10;
+  var deadContract = `Returned values aren't valid, did it run Out of Gas? You might also see this error if you are not using the correct ABI for the contract you are retrieving data from, requesting data from a block number that does not exist, or querying a node which is not fully synced.`;
 
   it("Initialized the contract with the correct values", function () {
     return MarkTokenSale.deployed()
@@ -26,33 +31,122 @@ contract("MarkTokenSale", function (accounts) {
   });
 
   it("Facilitate token buy", function () {
-    return MarkTokenSale.deployed().then(function (instance) {
-      tokenSaleInstance = instance;
-      return tokenSaleInstance
-        .buyTokens(numberOfTokens, {
-          from: buyer,
-          value: numberOfTokens * tokenPrice,
+    return MarkToken.deployed().then(function (instance) {
+      tokenInstance = instance;
+      return MarkTokenSale.deployed()
+        .then(function (instance) {
+          tokenSaleInstance = instance;
+          return tokenInstance.transfer(
+            tokenSaleInstance.address,
+            tokensAvailable,
+            { from: admin }
+          );
         })
         .then(function (receipt) {
-          assert.equal(receipt.logs.length, 1, "triggers one event");
-          assert.equal(receipt.logs[0].event, "Sell", "Should be a Sell event");
-          assert.equal(
-            receipt.logs[0].args._buyer,
-            accounts[1],
-            "logs the account that purchased the tokens"
-          );
-          assert.equal(
-            receipt.logs[0].args._amount,
-            numberOfTokens,
-            "increments the number of tokens sold"
-          );
-          return tokenSaleInstance.tokenSold();
+          return tokenSaleInstance
+            .buyTokens(numberOfTokens, {
+              from: buyer,
+              value: numberOfTokens * tokenPrice,
+            })
+            .then(function (receipt) {
+              assert.equal(receipt.logs.length, 1, "triggers one event");
+              assert.equal(
+                receipt.logs[0].event,
+                "Sell",
+                "Should be a Sell event"
+              );
+              assert.equal(
+                receipt.logs[0].args._buyer,
+                accounts[1],
+                "logs the account that purchased the tokens"
+              );
+              assert.equal(
+                receipt.logs[0].args._amount,
+                numberOfTokens,
+                "increments the number of tokens sold"
+              );
+              return tokenSaleInstance.tokenSold();
+            })
+            .then(function (amount) {
+              assert.equal(
+                amount.toNumber(),
+                numberOfTokens,
+                "Increments the number of tokens sold"
+              );
+              return tokenInstance.balanceOf(buyer);
+            })
+            .then(function (balance) {
+              assert.equal(balance.toNumber(), numberOfTokens);
+              return tokenInstance.balanceOf(tokenSaleInstance.address);
+            })
+            .then(function (balance) {
+              assert.equal(
+                balance.toNumber(),
+                tokensAvailable - numberOfTokens
+              );
+              return tokenSaleInstance.buyTokens(numberOfTokens, {
+                from: buyer,
+                value: 1,
+              });
+            })
+            .then(assert.fail)
+            .catch(function (error) {
+              assert(
+                error.message.indexOf("revert") >= 0,
+                "msg.value must be equal to the number of tokens in wei"
+              );
+              return tokenSaleInstance.buyTokens(999999, {
+                from: buyer,
+                value: 1,
+              });
+            })
+            .then(assert.fail)
+            .catch(function (error) {
+              assert(
+                error.message.indexOf("revert") >= 0,
+                "msg.value must be equal or less to the amount of tokens to be purchased"
+              );
+            });
+        });
+    });
+  });
+
+  it("Ends token sale", function () {
+    return MarkToken.deployed().then(function (instance) {
+      tokenInstance = instance;
+      return MarkTokenSale.deployed()
+        .then(function (instance) {
+          tokenSaleInstance = instance;
+          return tokenSaleInstance.endSale({ from: buyer });
         })
-        .then(function (amount) {
+        .then(assert.fail)
+        .catch(function (error) {
+          assert(
+            error.message.indexOf(
+              "revert" >= 0,
+              "Only the admin can end the sale"
+            )
+          );
+          return tokenSaleInstance.endSale({ from: admin });
+        })
+        .then(function (receipt) {
+          return tokenInstance.balanceOf(admin);
+        })
+        .then(function (balance) {
           assert.equal(
-            amount.toNumber(),
-            numberOfTokens,
-            "Increments the number of tokens sold"
+            balance.toNumber(),
+            999990,
+            "Retunrs all unsold MarkTokens to the admin"
+          );
+          return tokenSaleInstance.tokenPrice();
+        })
+        .then(assert.fail)
+        .catch(function (error) {
+          assert(
+            error.message.indexOf(
+              deadContract >= 0,
+              "Contract was not destroyed"
+            )
           );
         });
     });
